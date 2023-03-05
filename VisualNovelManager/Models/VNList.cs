fb59188200e0327842f463764984752cbf4e7e95
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2023-03-04 16:26:05 -0800

    Created user lists model

---
+++ VisualNovelManager/Models/VNList.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace VisualNovelManager.Models
+{
+    public class VNList
+    {
+        [Key]
+        public int ListId { get; set; }                 // Database ID
+
+        [Required]
+        public string UserId { get; set; }              // Which user in database created the list
+
+        [Required]
+        public string ListName { get; set; }            // Name of the created list
+
+        public string ListDescription { get; set; }     // Brief Description of list contents
+
+        [Required]
+        public string List { get; set; }                // String of Visual Novels added to the list
+
+    }
+}
